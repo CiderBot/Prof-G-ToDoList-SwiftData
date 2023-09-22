1f46fbce8b9ca7ed2c2573cea59d7c6bff27a6f8
Steven Yung <steve@redricebowl.com>
2023-09-21 23:23:28 -0700

    Initial Commit

---
+++ ToDoList-SwiftData/ToDoList_SwiftDataApp.swift
@@ -0,0 +1,32 @@
+//
+//  ToDoList_SwiftDataApp.swift
+//  ToDoList-SwiftData
+//
+//  Created by Steven Yung on 9/21/23.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct ToDoList_SwiftDataApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
